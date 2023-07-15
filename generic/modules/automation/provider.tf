2f18dd3f905850652ba1a14f793e54d55b72b649
bia andersson <tobias.andersson@comeon.com>
2023-07-15 13:02:58 +0200

    terraform deploy pipeline initial setup

--- generic/modules/automation/provider.tf
+++ generic/modules/automation/provider.tf
@@ -1,16 +1,11 @@
-variable "path" {
-  description = "Path to the google service account credentials file"
-  default     = "secrets/"
-}
-
 provider "google" {
   project     = "quickstart-terraform"
   region      = "europe-north1"
-  credentials = file("${var.path}/secret.json")
+  credentials = file("secret.json")
 }
 
 provider "google-beta" {
   project     = "quickstart-terraform"
   region      = "europe-north1"
-  credentials = file("${var.path}/secret.json")
+  credentials = file("secret.json")
 }
