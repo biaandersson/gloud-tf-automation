fa7d84ce69da1d6a89bc7945034547d962b01ded
bia andersson <tobias.andersson@comeon.com>
2023-07-15 13:23:18 +0200

    added secret in GIT

--- generic/modules/automation/provider.tf
+++ generic/modules/automation/provider.tf
@@ -1,11 +1,11 @@
 provider "google" {
-  project     = "quickstart-terraform"
-  region      = "europe-north1"
-  credentials = file("secrets/secret.json")
+  project = "quickstart-terraform"
+  region  = "europe-north1"
+  # credentials = file("secrets/secret.json")
 }
 
 provider "google-beta" {
-  project     = "quickstart-terraform"
-  region      = "europe-north1"
-  credentials = file("secrets/secret.json")
+  project = "quickstart-terraform"
+  region  = "europe-north1"
+  # credentials = file("secrets/secret.json")
 }
