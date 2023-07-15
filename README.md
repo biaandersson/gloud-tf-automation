c8ab935720d1148764d53e5aa08c451d6dac0fcc
bia andersson <tobias.andersson@comeon.com>
2023-07-15 17:01:05 +0200

    added README

---
+++ README.md
@@ -0,0 +1,59 @@
+# GCP Terraform Automation
+
+## Table of Contents
+
+- [About](#about)
+- [Getting Started](#getting_started)
+- [Usage](#usage)
+
+## About <a name = "about"></a>
+
+Terraform module to create GCP instance infrastructure and install saltstack on the instance.
+
+## Getting Started <a name = "getting_started"></a>
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See [deployment](#usage) for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+What things you need to install the software and how to install them.
+
+```bash
+git clone git@github.com:biaandersson/gloud-tf-automation.git
+cd gcloud-tf-automation
+```
+
+### Infrastructure
+
+Change the hostname in the `main.tf` file to your own instance name. You'll have to create a service account key in GCP and download the json file. Then change the `GOOGLE_CREDENTIALS` secret in the GitHub repo to the contents of the json file.
+
+You also need to have your own ssh key pair. Change the `ssh-keys` secret in the GitHub repo to the contents of your public key. The private key will be used to ssh into the instance. Remember to change the `/gcloud-projects/generic/modules/automation/scripts/` to set your onw ssh keys.
+
+```bash
+module "automation" {
+  source        = "./generic/modules/automation"
+  instance_name = ["test-example01", "test-example02"]
+  salt_master   = false
+}
+```
+
+If you are creating a salt master, change the `salt_master` variable to `true`.
+
+```bash
+
+module "automation" {
+  source        = "./generic/modules/automation"
+  instance_name = ["test-saltmaster01"]
+  salt_master   = true
+}
+```
+
+## Usage <a name = "usage"></a>
+
+Upon commit to the main branch, the CI/CD pipeline will run and create the infrastructure in GCP.
+It will install the following software on the instance:
+
+- [x] Saltstack Master
+- [x] Configure the master config file
+- [x] Install the salt-minion(if not a salt master)
+- [x] clone the saltstack repo
--- generic/modules/automation/scripts/saltmaster.sh
+++ generic/modules/automation/scripts/saltmaster.sh
@@ -12,19 +12,10 @@ sudo apt-get install salt-master -y
 # add ssh key
 sudo mkdir -p /root/.ssh
 
-sudo echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC/TB/1qVDU0TB1AskDRHPIiFWkD8irkT2i7OGVEVUNL0OAJO3yVjmJvQgbpb5mEGoTXyXD2iq6lLFcULbUabutce0+Qdw4LOiIgc7JVpgtXSOXY5VfNd0x5Kv8QiCkMAFmIJY5PWOtYxIPyQX7FZ6doM1aEgVOup4L9Gc9rZj7Q7hsQOlUa44fjJvE7yIJjqAFFZeV/OsjikD02/YhaJf82t5YNe6730zrl4DR7oYsFFjrQ6UdC/kxvLJBQ1SGXLsOOxrdl+8otQj9AeXHaqxv+4MqQgyJx51Vt9YdJvxJmb+AydVQvBU2mOvxJfK907LBh59CTXqUwEZoGsJiztzwKLkYXIU0J2OqOi6vskNwJMIs2j2uMSbNBkT1UfIEJNk42MkOihcFeIdo9/OHNJGiDdAyys9rLaawVJ8hQIA1Mcw8+JEJYQpfnn/fnZeiSJhHhlUsKAomNmDlV9qWsny1sG+ixtpSOVU9PfuJ+0JvI0agVAwloPeBwxIQ2WjH+iPk4eFUUc1xhMxXoX/Rg3K1ZsUW1vxBDUWxr4D4LQvGQ5HP3320z5tl8dyAFd79QOCNKFG9vtoGcI1MXZioco4FSx9F8f/46x+AnCjwIQo7YfXydH0HZ1SS3Gy3aaMoF0GWn57nOKMGbqJWZmgJV/ZPROXFlOAPXyCioyMRP1vazw== tobias.andersson@X6NPK97HN2.local
-" > /root/.ssh/authorized_keys
+sudo echo "public-key-goes-here" > /root/.ssh/authorized_keys
 
 # create the salt-deploy key
-echo "-----BEGIN OPENSSH PRIVATE KEY-----
-b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAaAAAABNlY2RzYS
-1zaGEyLW5pc3RwMjU2AAAACG5pc3RwMjU2AAAAQQR+WRe7xDppP6QSw7U2EV5C3ciduXJd
-E7vAEainl6sQZvwWhgBpnkp7ju2pc0aIDwIZYqB7vcp1IuOVjFrflXm4AAAAwNKcG3PSnB
-tzAAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBH5ZF7vEOmk/pBLD
-tTYRXkLdyJ25cl0Tu8ARqKeXqxBm/BaGAGmeSnuO7alzRogPAhlioHu9ynUi45WMWt+Veb
-gAAAAgE/I1nAVO6iOAN+NV/qEj+ax/rXJbgjjE/Y8gs3YMJzkAAAAhdG9iaWFzLmFuZGVy
-c3NvbkBYNk5QSzk3SE4yLmxvY2FsAQIDBAUGBw==
------END OPENSSH PRIVATE KEY-----" > /root/.ssh/salt-deploy
+echo "key-goes-here" > /root/.ssh/salt-deploy
 
 # change the permissions
 sudo chmod 600 /root/.ssh/authorized_keys ; sudo chmod 600 /root/.ssh/salt-deploy
@@ -67,8 +58,5 @@ sudo echo "Host github.com
 # add the /etc/environment file
 echo "GIT_SSH_COMMAND='ssh -i /root/.ssh/salt-deploy -o IdentitiesOnly=yes'" > /etc/environment
 
-# cd /srv/
-cd /srv 
-
-#  clone the repo
-git clone git@github.com:biaandersson/saltstack.git
+# clone the saltstack repo
+cd /srv ; git clone git@github.com:biaandersson/saltstack.git
