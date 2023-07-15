067911196ab1e590ce15be5944b7d204e7bfe563
bia andersson <tobias.andersson@comeon.com>
2023-07-15 13:40:52 +0200

    lets see if this work

--- generic/modules/automation/provider.tf
+++ generic/modules/automation/provider.tf
@@ -1,11 +1,11 @@
 provider "google" {
-  project     = "quickstart-terraform"
-  region      = "europe-north1"
-  credentials = jsondecode(github_actions_secret.CLOUD_JSON)
+  project = "quickstart-terraform"
+  region  = "europe-north1"
+  # credentials = jsondecode(github_actions_secret.CLOUD_JSON)
 }
 
 provider "google-beta" {
-  project     = "quickstart-terraform"
-  region      = "europe-north1"
-  credentials = jsondecode(github_actions_secret.CLOUD_JSON)
+  project = "quickstart-terraform"
+  region  = "europe-north1"
+  # credentials = jsondecode(github_actions_secret.CLOUD_JSON)
 }
